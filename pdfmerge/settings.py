5cd2bc90c37357a3b7b6b2bdace929d126c305ff
rupin <rupin.chheda@gmail.com>
2019-06-07 15:15:07 +0530

    customuser

--- pdfmerge/settings.py
+++ pdfmerge/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
