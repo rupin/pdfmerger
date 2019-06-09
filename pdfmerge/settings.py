91bac5ee2dc78e1d804570737940f195160fe4ce
rupin <rupin.chheda@gmail.com>
2019-06-09 17:48:58 +0530

    Add Models to Admin

--- pdfmerge/settings.py
+++ pdfmerge/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
+LOGIN_REDIRECT_URL = '/'
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
