788965a96aca52a28239db23b5a844064fcc31b9
rupin <rupin.chheda@gmail.com>
2019-06-09 18:58:41 +0530

    Add Models to Admin

--- pdfmerge/urls.py
+++ pdfmerge/urls.py
@@ -24,5 +24,5 @@ from django.views.generic.base import TemplateView # new
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', views.getUsers, name='index'),
-	path('accounts/', include('django.contrib.auth.urls'))
+	path('', include('django.contrib.auth.urls'))
 ]
