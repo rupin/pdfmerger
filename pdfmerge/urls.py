45996c8f60bc3d0cf227dad81d5b263baa5133ed
rupin <rupin.chheda@gmail.com>
2019-06-11 15:27:30 +0530

    Custom Login URL

--- pdfmerge/urls.py
+++ pdfmerge/urls.py
@@ -24,6 +24,6 @@ from django.views.generic.base import TemplateView # new
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', views.getUsers, name='home'),
-	path('/login', views.loginForm, name='login'),
-	path('/loginForm', views.loginUser, name='loginsubmit'),
+	path('/login/', views.loginForm, name='login'),
+	path('/loginForm/', views.loginUser, name='loginsubmit'),
 ]
