106b6d264fb4b541e228d5c4ed0956a2ec63a9ce
rupin <rupin.chheda@gmail.com>
2019-06-14 14:30:58 +0530

    Text Types

--- oldfiles/pdfmergertest.py
+++ oldfiles/pdfmergertest.py
@@ -1,13 +1,24 @@
 from PyPDF2 import PdfFileReader,PdfFileWriter
 import sys
-pdfs=["svg_on_canvas.pdf"]
-#print (f, k)
-base = PdfFileReader(open("form1.pdf", "rb"))
-basePage=base.getPage(0)
+
+
+EmptyForm = PdfFileReader(open("form1.pdf", "rb"))
+dataLayer=PdfFileReader(open("svg_on_canvas.pdf", "rb"))
+emptyFormPageCount=EmptyForm.getNumPages()
+dataLayerPagecount=dataLayer.getNumPages()
 output = PdfFileWriter()
-for pdf in pdfs:    
-    file = PdfFileReader(open(pdf, "rb"))    
-    basePage.mergePage(file.getPage(0))
-output.addPage(basePage)
+if(emptyFormPageCount>dataLayerPagecount):
+	for pageIndex in range(0,dataLayerPagecount):
+		dataPage=dataLayer.getPage(pageIndex)
+		formPage=EmptyForm.getPage(pageIndex)
+		formPage.mergePage(dataPage)
+		output.addPage(formPage)
+
+for emptyPagesIndex in range(dataLayerPagecount, emptyFormPageCount):
+
+ 	formPage=EmptyForm.getPage(emptyPagesIndex)
+ 	output.addPage(formPage)
+    
+
 with open("join.pdf", "wb") as outputStream:
     output.write(outputStream)
--- oldfiles/svgcanvas.py
+++ oldfiles/svgcanvas.py
@@ -8,23 +8,32 @@ pdfCellXOffset=(16.9/2)-(fontSize/4)
 pdfCellYOffset=(19.86/2)-(fontSize/4)
  
 def addText(FieldData):
-    my_canvas = canvas.Canvas('svg_on_canvas.pdf', pagesize=A4) 
-    for field in FieldData:
-    	#print(field)
-    	fieldText=field["text"].upper()
-    	letterCount=0
-    	for letter in fieldText:
-    		textobject = my_canvas.beginText()
-    		textobject.setFont('Courier', fontSize)
-    		xPos=field["x"]+(letterCount*field["h-inc"])
-    		textobject.setTextOrigin(xPos, field["y"])
-    		#textobject.setCharSpace(10.1)
-    		textobject.textLine(letter)
-    		my_canvas.drawText(textobject)
-    		letterCount=letterCount+1
-    
+	my_canvas = canvas.Canvas('svg_on_canvas.pdf', pagesize=A4) 
+	lastFieldPage=0
+	for field in FieldData:
+		#print(field)
+		fieldText=field["text"].upper()
+		letterCount=0
+		currentPage=field["page"]
+		if(currentPage==lastFieldPage+1):
+			lastFieldPage=currentPage
+			my_canvas.showPage()# change to next page 
 
-    my_canvas.save()
+		for letter in fieldText:
+			textobject = my_canvas.beginText()
+			textobject.setFont('Courier', fontSize)
+			xPos=field["x"]+(letterCount*field["h-inc"])
+			textobject.setTextOrigin(xPos, field["y"])
+			#textobject.setCharSpace(10.1)
+			textobject.textLine(letter)
+			my_canvas.drawText(textobject)
+			letterCount=letterCount+1	
+
+	#
+	
+		
+
+	my_canvas.save()
  
 if __name__ == '__main__':
 	newField={}
@@ -33,6 +42,8 @@ if __name__ == '__main__':
 	newField["y"]=676+pdfCellYOffset
 	newField["text"]="Rupin Raghavji Chheda"
 	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
 	fieldData.append(newField)
 
 	newField={}
@@ -40,34 +51,28 @@ if __name__ == '__main__':
 	newField["y"]=494+pdfCellYOffset
 	newField["text"]="India"
 	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
 	fieldData.append(newField)
 
 	newField={}
 	newField["x"]=107+pdfCellXOffset
 	newField["y"]=566+pdfCellYOffset
-	newField["text"]="11"
-	newField["h-inc"]=17
+	newField["text"]="11 10 1984"
+	newField["h-inc"]=16.5
+	newField["page"]=0
+	newField["type"]="block-text-date"
 	fieldData.append(newField)
 
-	newField={}
-	newField["x"]=157+pdfCellXOffset
-	newField["y"]=566+pdfCellYOffset
-	newField["text"]="10"
-	newField["h-inc"]=17
-	fieldData.append(newField)
-
-	newField={}
-	newField["x"]=207+pdfCellXOffset
-	newField["y"]=566+pdfCellYOffset
-	newField["text"]="1984"
-	newField["h-inc"]=17
-	fieldData.append(newField)
+	
 
 	newField={}
 	newField["x"]=135+pdfCellXOffset
 	newField["y"]=466+pdfCellYOffset
 	newField["text"]="Maharastra"
 	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
 	fieldData.append(newField)
 
 	newField={}
@@ -75,6 +80,8 @@ if __name__ == '__main__':
 	newField["y"]=436+pdfCellYOffset
 	newField["text"]="indian"
 	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
 	fieldData.append(newField)
 
 	newField={}
@@ -82,7 +89,211 @@ if __name__ == '__main__':
 	newField["y"]=408+pdfCellYOffset
 	newField["text"]="indian"
 	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
 	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=205+pdfCellXOffset
+	newField["y"]=367+pdfCellYOffset
+	newField["text"]="passport"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=205+pdfCellXOffset
+	newField["y"]=310+pdfCellYOffset
+	newField["text"]="f6612779"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=128+pdfCellXOffset
+	newField["y"]=282+pdfCellYOffset
+	newField["text"]="05 06 2006"
+	newField["h-inc"]=16.5
+	newField["page"]=0
+	newField["type"]="block-text-date"
+	fieldData.append(newField)
+
+
+	newField={}
+	newField["x"]=392+pdfCellXOffset
+	newField["y"]=282+pdfCellYOffset
+	newField["text"]="11 10 1984"
+	newField["h-inc"]=16.5
+	newField["page"]=0
+	newField["type"]="block-text-date"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=129+pdfCellXOffset
+	newField["y"]=231+pdfCellYOffset
+	newField["text"]="india"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=129+pdfCellXOffset
+	newField["y"]=206+pdfCellYOffset
+	newField["text"]="mumbai"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=188+pdfCellXOffset
+	newField["y"]=159+pdfCellYOffset
+	newField["text"]="india"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=188+pdfCellXOffset
+	newField["y"]=133+pdfCellYOffset
+	newField["text"]="maharastra"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=188+pdfCellXOffset
+	newField["y"]=104+pdfCellYOffset
+	newField["text"]="kandivali"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=188+pdfCellXOffset
+	newField["y"]=77+pdfCellYOffset
+	newField["text"]="mumbai"
+	newField["h-inc"]=17
+	newField["page"]=0
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=80+pdfCellXOffset
+	newField["y"]=57+pdfCellYOffset
+	newField["text"]="202 radhika darshan hemukalani cross road 2"
+	newField["h-inc"]=9
+	newField["page"]=0
+	newField["type"]="free-text"
+	fieldData.append(newField)
+
+
+	
+
+
+	newField={}
+	newField["x"]=99.9+pdfCellXOffset
+	newField["y"]=751+pdfCellYOffset
+	newField["text"]="Engineer"
+	newField["h-inc"]=17
+	newField["page"]=1
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=270+pdfCellXOffset
+	newField["y"]=726+pdfCellYOffset
+	newField["text"]="Bachelor in engineering"
+	newField["h-inc"]=9
+	newField["page"]=1
+	newField["type"]="free-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=135+pdfCellXOffset
+	newField["y"]=703+pdfCellYOffset
+	newField["text"]="Jain"
+	newField["h-inc"]=9
+	newField["page"]=1
+	newField["type"]="free-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=30.765+pdfCellXOffset
+	newField["y"]=413.65+pdfCellYOffset
+	newField["text"]="16"
+	newField["h-inc"]=17
+	newField["page"]=1
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=166+pdfCellXOffset
+	newField["y"]=413.65+pdfCellYOffset
+	newField["text"]="14"
+	newField["h-inc"]=17
+	newField["page"]=1
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=94+pdfCellXOffset
+	newField["y"]=387+pdfCellYOffset
+	newField["text"]="16 ah hood road"
+	newField["h-inc"]=17
+	newField["page"]=1
+	newField["type"]="block-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=104+pdfCellXOffset
+	newField["y"]=681+pdfCellYOffset
+	newField["text"]="Leisure and Travel"
+	newField["h-inc"]=9
+	newField["page"]=1
+	newField["type"]="free-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=323+pdfCellXOffset
+	newField["y"]=658+pdfCellYOffset
+	newField["text"]="1st November 2018"
+	newField["h-inc"]=9
+	newField["page"]=1
+	newField["type"]="free-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=102+pdfCellXOffset
+	newField["y"]=366+pdfCellYOffset
+	newField["text"]="Ramada Singapore"
+	newField["h-inc"]=9
+	newField["page"]=1
+	newField["type"]="free-text"
+	fieldData.append(newField)
+
+	newField={}
+	newField["x"]=279+pdfCellXOffset
+	newField["y"]=529+pdfCellYOffset
+	newField["text"]="indian"
+	newField["h-inc"]=9
+	newField["page"]=1
+	newField["type"]="free-text"
+	fieldData.append(newField)
+
+
+
+	
+
+
+
+
 
 	
 	#print(fieldData) 
