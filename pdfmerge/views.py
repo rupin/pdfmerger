855b1395ed8521582ab7e7cee5f3de6d58dc20ec
rupin <rupin.chheda@gmail.com>
2019-06-18 20:37:16 +0530

    Updated Model

--- pdfmerge/views.py
+++ pdfmerge/views.py
@@ -52,7 +52,7 @@ def fillForm(request):
 	# newField["type"]="block-text"
 	# fieldData.append(newField)
 
-	querysetdata=PDFFormField.objects.all().values('pdf__pdf_name')
+	querysetdata=PDFFormField.objects.all().values('pdf__pdf_name', 'pdf__generatedpdf__date_created')
 
 	dprint.dprint(querysetdata)
 
