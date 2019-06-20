95d9cef1281615f3e94614f85faf197fcc578e8e
rupin <rupin.chheda@gmail.com>
2019-06-20 18:56:22 +0530

    Fields Altered

--- pdfmerge/admin.py
+++ pdfmerge/admin.py
@@ -16,13 +16,7 @@ class PDFFormAdmin(admin.ModelAdmin):
 
 class PDFFormFieldAdmin(admin.ModelAdmin):
 	model=PDFFormField
-	list_display=['getPDFFormName', "getFieldDescription"]
-	def getPDFFormName(self, request):
-		a=super(PDFFormFieldAdmin,self).get_queryset(request).select_related('pdf').
-		return a.pdf_name
-
-	def getFieldDescription(self, request):
-		return super(PDFFormFieldAdmin,self).get_queryset(request).select_related('field').only("field__field_description")
+	list_display=['pdf', "field"]
 
     	
     
