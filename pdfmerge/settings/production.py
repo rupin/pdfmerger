f6e01b8860096fb1e58c15e149d6c6824cd7e9b2
rupin <rupin.chheda@gmail.com>
2019-07-07 19:51:21 +0530

    Added Login and Matching form to Profile

--- pdfmerge/settings/production.py
+++ pdfmerge/settings/production.py
@@ -158,7 +158,7 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'pdfmergefiles'
 service_account_info = json.loads(os.environ.get('GS_ACCOUNT_JSON', default=False))
-print(service_account_info)
-GS_CREDENTIALS = credentials = service_account.Credentials.from_service_account_info(service_account_info)
+#print(service_account_info)
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(service_account_info)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
