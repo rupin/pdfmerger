7cfef3b2a519555ca5c1bb61c9b81525d1f2e4d7
rupin <rupin.chheda@gmail.com>
2019-07-17 09:36:11 +0530

    Columns arrangement

--- pdfmerge/settings/local.py
+++ pdfmerge/settings/local.py
@@ -126,12 +126,12 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/'
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '192.168.1.101']
 
 
 # Static files (CSS, JavaScript, Images)
